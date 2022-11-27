ad96af31a28906f81a24fad0b1fe8ca6891e85c0
Kristina136 <criska.2010@yandex.ru>
2022-11-27 18:37:16 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import "./App.css";
+import About from "./About";
+ import Home from "./Home";
+ import Contact from "./Contact";
+
+
+function App() {
+
+  return( <Router >
+  <nav>
+    <Link className="link" to="/">Home </Link>
+<Link className="link" to="/about">About </Link>
+<Link className="link" to="/contact">Contact </Link>
+  </nav>
+  <Routes>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/contact" element={<Contact/>}/>
+  </Routes>
+</Router>
+)
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+function Button({anyButton}){
+    return(
+        <div>
+<button onClick={()=>anyButton("Dominican Republik")}>Dominican Republik</button>
+<button>Indonesia</button>
+<button>Poland</button>
+<button>Ausria</button>
+        </div>
+    )
+}
+export default Button
